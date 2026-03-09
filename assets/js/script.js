8da982267e7441b361a8291905a2a906f80f3204
“choumiryou” <ningyinheize@gmail.com>
2026-03-09 21:57:30 +0900

    script.jsの辞書データパスを修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ if (lists.length > 0) {
 }
 
 // --- 初期化処理 (Kuromojiのロード) --- 【変更】ローカルの辞書ファイルを参照するようにパスを修正
-const DIC_URL = "../assets/dict/";
+const DIC_URL = "assets/dict/";
 
 kuromoji.builder({ dicPath: DIC_URL }).build(function (err, _tokenizer) {
   if (err) {
