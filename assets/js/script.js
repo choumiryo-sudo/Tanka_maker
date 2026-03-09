c13a5672594d7f925e3e64de898bfcaa0938e028
“choumiryou” <ningyinheize@gmail.com>
2026-03-09 22:12:39 +0900

    パスにスペルミス

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ if (lists.length > 0) {
 }
 
 // --- 初期化処理 (Kuromojiのロード) ---
-const DIC_URL = "https://takuyaa.github.io/kuromoji.js/dict//dict/";
+const DIC_URL = "https://takuyaa.github.io/kuromoji.js/dict/";
 
 kuromoji.builder({ dicPath: DIC_URL }).build(function (err, _tokenizer) {
   if (err) {
